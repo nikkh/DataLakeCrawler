ea570ea60505311e1dd36867169d09988fba58f1
nhill <nhill@MININT-CU56C8E>
2021-03-10 09:43:35 +0000

    try explicit message completion

--- DataLakeCrawler/Laker.cs
+++ DataLakeCrawler/Laker.cs
@@ -5,17 +5,22 @@ using Azure.Storage.Files.DataLake;
 using Azure.Storage.Files.DataLake.Models;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.ServiceBus.Core;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.EventHubs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -51,10 +56,51 @@ namespace DataLakeCrawler
             _queueClient = new QueueClient(csb);
         }
 
-        [FunctionName("ProcessLakeFolder")]
+
+        [FunctionName("LakerTrigger")]
+        public async Task<IActionResult> Run(
+    [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+    ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            var directoryClient = fileSystemClient.GetDirectoryClient(name);
+            log.LogDebug($"Processing top level directory {name}");
+            
+            AsyncPageable<PathItem> pathItems = directoryClient.GetPathsAsync(false);
+            await foreach (var pathItem in pathItems)
+            {
+                if ((bool)pathItem.IsDirectory)
+                {
+
+                    Console.WriteLine($"{pathItem.Name} is a directory.  Add to processing queue.");
+                    string pathItemData = JsonConvert.SerializeObject(pathItem);
+                    Message message = new Message(Encoding.UTF8.GetBytes(pathItemData));
+                    await _queueClient.SendAsync(message);
+                }
+                else
+                {
+                    // do nothing (files will be handled by the main function
+                }
+            }
+
+            string responseMessage = $"Processing triggered for {name}";
+
+            return new OkObjectResult(responseMessage);
+        }
+    
+
+    [FunctionName("ProcessLakeFolder")]
        // [return: EventHub("lakehub", Connection = "EventHubConnection")]
         public async Task Run([ServiceBusTrigger("lake-queue", Connection = "ServiceBusConnection")] Message message, ILogger log, MessageReceiver messageReceiver)
         {
+            
             CrawlerResult cr = new CrawlerResult();
             Stopwatch w = new Stopwatch();
             string payload = System.Text.Encoding.UTF8.GetString(message.Body);
@@ -71,6 +117,7 @@ namespace DataLakeCrawler
             {
                 Exception e = new Exception("Laker invoked by passing a file. This is not supported!");
                 telemetryClient.TrackException(e);
+                await messageReceiver.DeadLetterAsync(message.SystemProperties.LockToken);
                 throw e;
             }
             w.Start();
@@ -115,6 +162,7 @@ namespace DataLakeCrawler
                     cr.Files.Add(cf);
                 }
             }
+            await messageReceiver.CompleteAsync(message.SystemProperties.LockToken);
             return;
         }
     }
